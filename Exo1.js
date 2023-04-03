3fac1b6e340c8dcec9bc270454fb596ad0e42a1c
vegAs-RW <jordan.pouchot2@gmail.com>
2023-04-03 12:17:54 +0200

    exo1

---
+++ Exo1.js
@@ -0,0 +1,21 @@
+/*let num = Math.floor(Math.random() * 100) +1;*/
+let num = 2
+let nbTry = 0
+
+
+process.stdin.on('data', (input) => {
+    nbTry++
+    if (input < num) {
+        console.log('le nombre est plus grand');
+    }
+    if (input > num) {
+        console.log('le nombre est plus petit');
+    }
+    if (input == num) {
+        console.log(`Correct, vous avez trouvé en ${nbTry}`);
+    }
+    if (nbTry == 10) {
+        console.log("Perdu");
+    }
+    process.exit(0);
+})
