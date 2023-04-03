c32999e52bde6f7d8ecd6d31fc2ff280b2750e56
vegAs-RW <jordan.pouchot2@gmail.com>
2023-04-03 10:23:50 +0200

    more test

--- demo.js
+++ demo.js
@@ -4,3 +4,25 @@ console.table({
 });
 
 console.log('hello Node.js');
+
+
+// Exemple code synchrone
+const showResult = (a,b) => {
+    console.log(square(sum(a,b)));
+};
+
+const sum = (a,b) => {
+    return a + b
+};
+
+const square = (a, p=2) => a**p;
+
+showResult (3,7);
+
+// Exemple code asynchrone
+console.log('start');
+// S'executera 1 seconde plus tard que les deux autres console.log
+setTimeout(() => console.log('hello world'), 1000); // Callback tache accomplie aprés l'evenement
+
+console.log('end')
+
