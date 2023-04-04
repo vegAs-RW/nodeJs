244219778b3ea25d42e40348df696bf4d0b48aa7
vegAs-RW <jordan.pouchot2@gmail.com>
2023-04-04 09:41:38 +0200

    modif

--- demo2.js
+++ demo2.js
@@ -7,9 +7,9 @@ console.log(
     `Ce mbp appartien à ${username} et a ${cpus}`
 );
 
-process.stdin.write('Bonjour \n')
+/*process.stdin.write('Bonjour \n')
 process.stdout.write('Bonjour \n')
-process.stderr.write('Error \n')
+process.stderr.write('Error \n')*/
 
 process.stdin.on('data', (chunk) => {
     process.exit(0);
