620968edaffdbfed85320fd5888f9e17725eeca2
vegAs-RW <jordan.pouchot2@gmail.com>
2023-04-06 14:55:54 +0200

    add day3 course

--- jour3/cours/stat.js
+++ jour3/cours/stat.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 const http = require('http');
 
-http.createServer()
+http.createServer(function(req, res) {
+    fs.readFile(__dirname + req.url, (err, data) => {
+        if (err) {
+            res.writeHead(400)
+            res.end(JSON.stringify(err))
+            return;
+        }
+
+        res.writeHead(200)
+        res.end(data)
+    })
+}).listen(8000)
