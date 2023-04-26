9d957378bdee80f2800640316b0c3fc52d18ed5f
vegAs-RW <jordan.pouchot2@gmail.com>
2023-04-26 10:55:51 +0200

    add users list and function for notify typing

--- Semaine2/jour1/Exo/index.js
+++ Semaine2/jour1/Exo/index.js
@@ -1,47 +1,49 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 const io = socketio(server);
 
 let users = [];
 
-io.on('connection', (socket) => {
-  console.log('Nouvelle connexion');
+io.on("connection", (socket) => {
+  console.log("Nouvelle connexion");
 
-  socket.on('new user', (username) => {
+  socket.on("new user", (username) => {
     socket.username = username;
     users.push(username);
     console.log(`Salut ${username}, commence a discuter`);
-    io.emit('users', users);
+    io.emit("users", users);
   });
 
-  socket.on('chat message', (message) => {
-    io.emit('chat message', { username: socket.username, message });
+  socket.on("chat message", (message) => {
+    io.emit("chat message", { username: socket.username, message });
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`${socket.username} c'est déco`);
     users = users.filter((user) => user !== socket.username);
-    io.emit('users', users);
+    io.emit("users", users);
+  });
+
+  socket.on("notifyWritting", () => {
+    io.emit("notifyWritting", socket.username);
   });
 });
-
 
 const PORT = 9000;
 server.listen(PORT, () => {
   console.log(`Le serveur écoute sur le port ${PORT}`);
 });
-
--- Semaine2/jour1/Exo/public/scripts/app.js
+++ Semaine2/jour1/Exo/public/scripts/app.js
@@ -1,37 +1,102 @@
 const socket = io();
 
-const messageForm = document.getElementById('message-form');
-const messageInput = document.getElementById('message-input');
-const messageList = document.getElementById('messages-list');
-const userList = document.getElementById('user-list');
-const channelList = document.getElementById('channel-list');
-const channels = document.querySelectorAll('.channel-link');
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
+const messageList = document.getElementById("messages-list");
+const userList = document.getElementById("user-list");
+const channelList = document.getElementById("channel-list");
+const channels = document.querySelectorAll(".channel-link");
 
-const username = prompt('Quel est votre nom d\'utilisateur ?');
+let activeChannel = "general";
 
-socket.emit('new user', username);
+// Function to switch channels
+function switchChannel(channel) {
+  activeChannel = channel;
+  channels.forEach((c) => c.parentElement.classList.remove("active-channel"));
+  const selectedChannel = document.querySelector(
+    `a[data-channel="${channel}"]`
+  );
+  selectedChannel.parentElement.classList.add("active-channel");
+  messageList.innerHTML = "";
+  const channelHeading = document.querySelector("h1");
+  channelHeading.textContent = `#${channel} channel`;
+}
 
-socket.on('users', (users) => {
-  userList.innerHTML = '';
+// Switch to the 'general' channel by default
+switchChannel("general");
+
+channels.forEach((link) => {
+  link.addEventListener("click", () => {
+    const channel = link.dataset.channel;
+    switchChannel(channel);
+  });
+});
+
+const username = prompt("What is your username?");
+
+socket.emit("new user", username);
+
+socket.on("users", (users) => {
+  userList.innerHTML = "";
   users.forEach((user) => {
-    const userItem = document.createElement('li');
+    const userItem = document.createElement("li");
     userItem.textContent = user;
     userList.appendChild(userItem);
   });
 });
 
-messageForm.addEventListener('submit', (event) => {
+messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const message = messageInput.value;
-  socket.emit('chat message', message);
-  messageInput.value = '';
+  //console.log( { message, channel: activeChannel });
+  socket.emit("chat message", { message, channel: activeChannel });
+  messageInput.value = "";
 });
 
-socket.on('chat message', (data) => {
-  const messageItem = document.createElement('li');
-  messageItem.innerHTML = `<strong>${data.username}</strong>: ${data.message}`;
-  messageList.appendChild(messageItem);
+socket.on("chat message", (data) => {
+  console.log(data);
+  if (data.message.channel === activeChannel) {
+    const messageItem = document.createElement("li");
+    messageItem.innerHTML = `<strong>${data.username}</strong>: ${data.message.message}`;
+    messageList.appendChild(messageItem);
+  }
 });
 
+//Ajouter un gestionnaire d'événements pour chaque lien de channel
+channels.forEach((channel) => {
+  channel.addEventListener("click", (event) => {
+    // Empêcher le comportement par défaut des liens de se déplacer vers une nouvelle page
+    event.preventDefault();
 
+    // Récupérer le nom du channel à partir de l'attribut data-channel
+    const newChannel = event.target.getAttribute("data-channel");
 
+    // Mettre à jour la valeur de currentChannel
+    currentChannel = newChannel;
+    console.log(currentChannel);
+    // Mettre en surbrillance le lien de channel actif
+    const activeChannel = document.querySelector(".active-channel");
+    activeChannel.classList.remove("active-channel");
+    event.target.parentNode.classList.add("active-channel");
+
+    // Effacer la liste de messages actuelle
+    //messageList.innerHTML = "";
+
+    // Envoyer un message au serveur pour changer de channel
+    socket.emit("change channel", channel.textContent);
+  });
+});
+
+// Ecouter l'event d'ecriture
+messageInput.addEventListener("keypress", () => {
+  socket.emit("notifyWritting", { user: socket.username });
+});
+
+// ecriture
+socket.on("notifyWritting", (username) => {
+  const writingDiv = document.getElementById("writing");
+  writingDiv.textContent = username + " is typing...";
+  setTimeout(() => {
+    writingDiv.textContent = "";
+  }, 3000);
+});
